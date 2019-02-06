b39e017908c09bcb71d5ced72d68e931388e4741
Austin <Austin_MacBook@Austins-MacBook-Pro.local>
2019-02-06 11:05:27 -0600

    code challenge

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,19 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from "./YourBotArmy"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+
+
 
   render() {
     return (
       <div>
         {/* put your components here */}
+        <YourBotArmy bots={this.props.bots} />
+        <BotCollection bots={this.props.bots} />
+
       </div>
     );
   }
